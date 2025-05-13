f43686419e660aad2b0e75a7fee9ab3af467fa14
yacs-2 <chromebook2@youngsvilleacademy.org>
2025-05-13 15:27:18 -0400

    Update ttt.js

--- wdsub/ttt.js
+++ wdsub/ttt.js
@@ -1,1 +1,53 @@
+let isXTurn = true;
 
+const squareArr = document.querySelectorAll(".square");
+
+for (const _element of squareArr) {
+  _element.addEventListener("click", markSquare);
+}
+
+
+
+function markSquare(_square) {
+   if (_square.srcElement.innerText !="X" && _square.srcElement.innerText != "O") {
+  if (isXTurn) {
+      _square.srcElement.innerText = "X"
+    } else {
+    _square.srcElement.innerText = "O";
+  }
+
+    if (checkComplete()) {
+      let winnerDiv = document.getElementById("winner");
+      winnerDiv.style.display = "block";
+      
+      let winnerText = document.querySelector("#winner div");
+      winnerDiv.innerText = isXTurn ? "Player X wins!" : "PLayer 0 wins!"
+    }
+
+  isXTurn = !isXTurn;
+
+  }
+}
+
+function checkComplete() {
+  for (let _i = 0; _i < 3; _i++) {
+    if ((squareArr[_i].innerText == "X" || squareArr[_i].innerText == "O") 
+      && squareArr[_i].innerText == squareArr[_i + 3].innerText
+    && squareArr[_i].innerText == squareArr[_i + 6].innerText
+    ) return true;
+     else if ((squareArr[3 * _i].innerText == "X" || squareArr[3 * _i].innerText == "O") 
+      && squareArr[3 * _i].innerText == squareArr[3 * _i + 1].innerText
+    && squareArr[3 * _i].innerText == squareArr[3 * _i + 2].innerText
+    ) return true;
+  }
+   
+ if ((squareArr[0].innerText == "X" || squareArr[0].innerText == "0") 
+    && squareArr[0].innerText == squareArr[4].innerText
+    && squareArr[0].innerText == squareArr[8].innerText)
+   return true;
+   else if ((squareArr[2].innerText == "X" || squareArr[2].innerText == "0")
+    && squareArr[2].innerText == squareArr[4].innerText
+    && squareArr[2].innerText == squareArr[6].innerText 
+    ) return true;
+  else return false;
+  }
