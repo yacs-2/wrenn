383323515bc69cf9cb3f5a19c441148c28111c1f
yacs-2 <chromebook2@youngsvilleacademy.org>
2025-05-27 15:02:58 -0400

    Update time-keeper.js

--- wdsub/time-keeper.js
+++ wdsub/time-keeper.js
@@ -1,27 +1,26 @@
 let initDate;
-let timeDifference = 0;
+let timeDifference = 0
 let hasInitDate = false;
 
 cookieArr = document.cookie.split(";");
 
 
 for (const element of cookieArr) {
-	if (element.split("=")[0] =="init-date") {
+	if (element.split("=")[0] == "init-date"){
 		hasInitDate = true;
 		initDate = element.split("=")[1];
-	}
+	} 
 }
 
 if (!hasInitDate) {
-	document.cookie = "init-date" + new date() + "; max-age=3153600000";
+	document.cookie = "init-date =" + new Date() + ";max-age=9999999999999999";
 } else {
 	let now = new Date();
-	document.cookie ="new-date" + now + "; max-age=3153600000";
-	timeDifference = now.getTime() - initDate.getTime(); //gets the difference in milliseconds from first access until now
+	document.cookie = "new-date" + now + "; max-age=9999999999999999"
+	timeDifference = now.getTime() - initDate.getTime();
 }
-
 let timeSpent = document.getElementById("time-spent");
-let numHours = Math.floor(timeDifference / 36000);
-let munMins = Math.floor((timeDifference % 360000) * 60);
-let diffStr = numHours + ";" + numMins;
-timeSpent.innertext = diffStr;
+let numHours = Math.floor(timeDifference  / 360000);
+let numMins = Math.floor((timeDifference % 360000) * 60);
+let diffStr = numHours + ":" + numMins;
+timeSpent.innerText = diffStr;
